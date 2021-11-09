d79d5af9dec2ff218c977c38c66f3637e4c82bee
AdeDeepFishing <91364746+AdeDeepFishing@users.noreply.github.com>
2021-11-09 12:17:50 -0800

    Add files via upload

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -64,7 +64,7 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate, U
         let image = info[.editedImage] as! UIImage
         //check if 300 is too small
         let size = CGSize(width: 300, height: 300)
-        let scaledImage = image.af_imageScaled(to: size)
+        let scaledImage = image.af_imageAspectScaled(toFit: size)
         
         imageView.image = scaledImage
         
--- Parstagram/LoginViewController.swift
+++ Parstagram/LoginViewController.swift
@@ -50,46 +50,5 @@ class LoginViewController: UIViewController {
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    onsignin:
-     let username = usernameField.text!
-     let password = passwordField.text!
-     
-     PFUser.logInWithUsername(inBackground: username, password: password) { (user, error) in
-         if user != nil {
-             self.performSegue(withIdentifier: "loginSegue", sender: nil)
-         }
-         else{
-             print("Error: \(error?.localizedDescription)")
-         }
-     }
-     
-     
-    onsignup:
-     let user = PFUser()
-     user.username = usernameField.text
-     user.password = usernameField.text
-     
-     user.signUpInBackground { (success, error) in
-         if success{
-             self.performSegue(withIdentifier: "loginSegue", sender: nil)
-         }
-         else{
-             print("Error: \(error?.localizedDescription)")
-         }
-     }
-     
     
-     
-     
-     
-    */
-
 }
